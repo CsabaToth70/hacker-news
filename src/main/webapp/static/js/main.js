6f866c8000e895f074c5aaf0fcb49cea6c841f42
CsabaToth70 <lesitocsa@gmail.com>
2021-03-07 08:08:51 +0100

    feat: add ordered newest news

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -24,10 +24,13 @@ async function displayHackerNews(url, actualPage, cardByPage) {
         allPageNumber++;
     }
     let newsCards = document.getElementById('contents');
-    newsCards.insertAdjacentHTML('beforeend',
-        "<button id=\"prev\" class=\"btn btn-outline-info btn-sm\" onclick=\"previous_page_single()\" >Previous</button>\n" +
-        "<button id=\"next\" class=\"btn btn-outline-info btn-sm\" onclick=\"next_page_single()\" >Next</button>\n" +
-    "<br><br>")
+    let apiSource = document.getElementById('whichApi').dataset.api;
+    if(apiSource !== "news"){
+        newsCards.insertAdjacentHTML('beforeend',
+            "<button id=\"prev\" class=\"btn btn-outline-info btn-sm\" onclick=\"previous_page_single()\" >Previous</button>\n" +
+            "<button id=\"next\" class=\"btn btn-outline-info btn-sm\" onclick=\"next_page_single()\" >Next</button>\n" +
+            "<br><br>")
+    }
     newsCards.insertAdjacentHTML('beforeend',
         "<p id=\"pageIndex\" data-page-number=" + actualPage + " data-all-page-number=" + allPageNumber + "></p>" +
         "<div class=\"card-columns\">\n" +
@@ -42,17 +45,23 @@ async function displayHackerNews(url, actualPage, cardByPage) {
     function drawCard(contents, startingCardNumber, numberOfCardsByPage){
         let cards = "";
         let counter = 0;
-        let pointList = [];
-        let numberOfLastCardByPage = startingCardNumber + numberOfCardsByPage
-        let apiSource = document.getElementById('whichApi').dataset.api
+        let idList = [];
+        let pointMapList = [];
+        let timeList = [];
+        let numberOfLastCardByPage = startingCardNumber + numberOfCardsByPage;
+        let apiSource = document.getElementById('whichApi').dataset.api;
         if(apiSource === "top"){
             for(let item of contents){
-                pointList.push(parseInt(item['points']));
+                if(item["points"] !== null){
+                    pointMapList.push({"id":item['id'], "point":parseInt(item['points'])});
+                } else {
+                    pointMapList.push({"id":item['id'], "point": 0});
+                }
             }
-            pointList.sort(function(a, b){return b-a});
-            for(let point of pointList){
+            idList = getIdList(pointMapList);
+            for(let id of idList){
                 for(let content of contents){
-                    if(point === parseInt(content['points']) ){
+                    if(id === parseInt(content['id']) ){
                         cards = cards +
                             `<div class="card bg-light mb-3" style="max-width: 18rem; id="${counter}" data-points="${content['points']}"> 
                       <div class="card-header"><a href="${content["url"]}"> ${content['title']}</a></div>
@@ -64,6 +73,45 @@ async function displayHackerNews(url, actualPage, cardByPage) {
                     }
                 }
                 counter++;
+            }
+        } else if(apiSource === "newest"){
+            for(let item of contents){
+                if(item["points"] !== null){
+                    pointMapList.push({"id":item['id'], "time_ago":item['time_ago']});
+
+                } else {
+                    pointMapList.push({"id":item['id'], "time_ago": "1000 days ago"});
+                }
+            }
+            idList = getNewestIdList(pointMapList);
+            for(let id of idList){
+                for(let content of contents){
+                    if(id === parseInt(content['id']) ){
+                        cards = cards +
+                            `<div class="card bg-light mb-3" style="max-width: 18rem; id="${counter}" data-points="${content['points']}"> 
+                      <div class="card-header"><a href="${content["url"]}"> ${content['title']}</a></div>
+                      <div class="card-body">
+                        <h5 class="card-title">${content['domain']}</h5>
+                        <p class="card-text">${content['time_ago']}</p>
+                      </div>
+                 </div>`;
+                    }
+                }
+                counter++;
+            }
+        } else if(apiSource === "job"){
+            for (let content of contents) {
+                if(startingCardNumber <= counter && counter < numberOfLastCardByPage){
+                    cards = cards +
+                        `<div class="card bg-light mb-3" style="max-width: 18rem; id="${counter}" data-points="${content['points']}"> 
+                      <div class="card-header"><a href="${content["url"]}"> ${content['title']}</a></div>
+                      <div class="card-body">
+<!--                        <h5 class="card-title">${content['domain']}</h5>-->
+                        <p class="card-text">${content['time_ago']}</p>
+                      </div>
+                 </div>`;
+                    counter++;
+                }
             }
         } else {
             for (let content of contents) {
@@ -90,5 +138,67 @@ async function displayHackerNews(url, actualPage, cardByPage) {
         alert("go to next page if it exists");
     }
 
+    function getIdList(pointMapList){
+    let idList = [];
+    for(let j=0; j<pointMapList.length - 1; j++){
+        for(let i=0; i<pointMapList.length - 1; i++){
+            if(pointMapList[i]["point"] < (pointMapList[i+1]["point"])){
+                let tmpMap = {"id":pointMapList[i+1]["id"],"point": pointMapList[i+1]["point"]};
+                pointMapList[i+1] = pointMapList[i];
+                pointMapList[i] = tmpMap;
+            }
+        }
+    }
+        for(let map of pointMapList){
+            idList.push(map["id"])
+        }
+    return idList;
+    }
+
+    function getNewestIdList(pointMapList){
+        let idList = [];
+        let index = 0;
+        for(let map of pointMapList){
+            pointMapList[index]["time_ago"] = getConvertedTimeAgo(map["time_ago"]);
+            index++;
+        }
+        for(let j=0; j<pointMapList.length - 1; j++){
+            for(let i=0; i<pointMapList.length - 1; i++){
+                if(pointMapList[i]["point"] > (pointMapList[i+1]["point"])){
+                    let tmpMap = {"id":pointMapList[i+1]["id"],"point": pointMapList[i+1]["point"]};
+                    pointMapList[i+1] = pointMapList[i];
+                    pointMapList[i] = tmpMap;
+                }
+            }
+        }
+        for(let map of pointMapList){
+            idList.push(map["id"])
+        }
+        return idList;
+    }
+    function getConvertedTimeAgo(time_ago){
+    let convertedValue = 0;
+    let stringList = [];
+    let index = 0;
+    // todo a/an/x day/days/hour/hours ago
+         stringList = time_ago.split(" ");
+         for(let item of stringList){
+             if(item === "a" || item === "an"){
+                 stringList[index] = 1;
+             } else if (!isNaN(item)){
+                 stringList[index] = parseInt(item);
+             } else if (item === "day" || item === "days"){
+                 stringList[index] = 1440;
+             } else if (item === "hours" || item === "hour"){
+                 stringList[index] = 60;
+             } else if (item === "minute" || item === "minutes"){
+                 stringList[index] = 1;
+             } else {
+                 stringList[index] = 0;
+             }
+         }
+         convertedValue = stringList[0] * stringList [1];
+    return convertedValue;
+    }
 
 init();
